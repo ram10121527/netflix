9f4e80cc05242fadadb3ba12e4b54c1592a9221a
Ram <ramanathan1210@gmail.com>
2024-03-07 21:22:32 +0530

    sign/sign & Login/header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import Body from "./components/Body";
+import './index.css';
 
 function App() {
   return (
-    <div className="text-xl font-bold text-green-800 ">
-      Namaste, Lets build login authentication form
-    </div>
+    <Body/>
   );
-}
+};
 
 export default App;
