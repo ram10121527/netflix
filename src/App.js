78de0d868d47fd46216505547d761d8f64700455
Ram <ramanathan1210@gmail.com>
2024-03-09 00:57:08 +0530

    validation && redux && auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Provider } from "react-redux";
 import Body from "./components/Body";
 import './index.css';
+import appStore from "./utils/appStore";
 
 function App() {
   return (
-    <Body/>
+   
+   <Provider store={appStore}> 
+   
+   <Body/>
+   
+   </Provider>
   );
 };
 
