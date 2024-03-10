6b5bb9295f9e2f43d3cf0588061c13f555460abb
Ram <ramanathan1210@gmail.com>
2024-03-10 10:54:35 +0530

    TMDB API data fetch && build main container

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { auth } from "../utils/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO } from "../utils/constants";
 
 const Header = () => {
   const dispatch=useDispatch();
@@ -41,7 +42,7 @@ const Header = () => {
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between" >
-        <img className="w-44" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
+        <img className="w-44" src={LOGO} alt="logo"/>
 
       {user && (<div className="flex p-2">
         <img className="w-12 h-12" alt="usericon" src={user?.photoURL}
